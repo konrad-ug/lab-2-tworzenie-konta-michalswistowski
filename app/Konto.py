2ec8b0fe9e1e18aecdab48e4f47c7e37320d302e
michalswistowski <michalswistowski2002@gmail.com>
2022-10-26 17:45:07 +0200

    feature 2,3,4,5

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,20 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, promocja = None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        self.promocja = promocja
+
+        self.warunek_peselu(pesel)
+        self.warunek_promocji(promocja)
+
+    def warunek_peselu(self, pesel):
+        if(len(pesel) == 11):
+            self.pesel = pesel
+        else:
+            self.pesel = "niepoprawny pesel"
+    
+    def warunek_promocji(self, promocja):
+        if(promocja != None and len(promocja) == 8 and promocja[:5] == "PROM_" and (int(self.pesel[:2]) > 60 or
+        int(self.pesel[2:4]) > 20)): 
+            self.saldo += 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -1,13 +1,36 @@
 import unittest
+import re
 
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
-
+    
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "11111111111")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "11111111111", "brak peselu!")
+        
 
-    #tutaj proszę dodawać nowe testy
+    def test_inna_dlugosc_pesel(self):
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "11111111111")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "niepoprawny pesel!")
+
+
+    def test_promocja_nieprawidlowo(self):
+        konto = Konto("Dariusz", "Januszewski", "11111111111", "PROM_BCDfg")
+        self.assertEqual(konto.saldo, 0, "mimo nieprawidlowego kodu dodano 50zl!")
+
+    def test_promocja_po60_przed2000(self):
+        konto = Konto("Dariusz", "Januszewski", "71111111111", "PROM_BCD")
+        self.assertEqual(konto.saldo, 50, "niedodano promocji!")
+
+    def test_promocja_po60_po2000(self):
+        konto = Konto("Dariusz", "Januszewski", "11301111111", "PROM_BCD")
+        self.assertEqual(konto.saldo, 50, "niedodano promocji!")
+
+
+    def test_promocja_przed60(self):
+        konto = Konto("Dariusz", "Januszewski", "58111111111", "PROM_BCDfg")
+        self.assertEqual(konto.saldo, 0, "nieslusznie dodano promocje!")
